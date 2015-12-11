5252f9de8348f24f8d1be2838106e71d2c716ea0
James Landy <jmlmdf@mail.missouri.edu>
2015-12-11 17:23:08 -0600

    Got rid of old code

--- hfyGetPosts.py
+++ hfyGetPosts.py
@@ -39,22 +39,3 @@ for submission in subreddit.get_top_from_month(limit=2):
 
 with open("test.txt", "w") as f:
             json.dump(story_list, f)
-#print (listoflists)
-
-#Call script that puts each post into a txt file
-
-#        posts_replied_to = f.read()
-#        posts_replied_to = posts_replied_to.split("\n")
-#        posts_replied_to = filter(None, posts_replied_to)
-
-#subreddit = r.get_subreddit('pythonforengineers')
-#for submission in subreddit.get_hot(limit=5):
-#    if submission.id not in posts_replied_to:
-#        if re.search("Please Read This First before posting anything here", submission.title, re.IGNORECASE):
-#            submission.add_comment("Yo ma man! Python is da best")
-#            print("Bot replying to : ", submission.title)
-#            posts_replied_to.append(submission.id)
-#with open("posts_replied_to.txt", "w") as f:
-#                for post_id in posts_replied_to:
-#                    f.write(post_id + "\n")
-
